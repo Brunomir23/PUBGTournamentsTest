424b0551c0253f301dfc60058d5e441c2ee38809
Brunomir <bferrari23@gmail.com>
2020-07-29 21:34:57 -0300

    UnityTest
    PUBG Tournaments List

---
+++ WidowGamesTest/Assets/Scripts/API.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using SimpleJSON;
+
+public class API : MonoBehaviour
+{
+    private const string URL = "https://api.pubg.com/tournaments";    
+    private const string api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5MzRiMzVkMC1iMzYxLTAxMzgtYmM2MS0wOTdjZTlhNDY4NjMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTk1OTgzMDM2LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImJmZXJyYXJpMjMtZ21hIn0.qaJpDmudzNhUk1bhtlpH_DI_KjEl8ZiL7mk0JKZENGc";
+    public Text countPageText, actualPageText;
+    public string json;
+    private List<Lista> ListaTorneos;
+
+    public Transform listaContainer;
+    public Transform listaTemplate;
+    public int pag, pagCount;
+  
+    private void Start()
+    {
+        Request();
+        pag = 0;
+    }
+    public void Request()
+    {
+        WWWForm form = new WWWForm();
+
+        Dictionary<string, string> headers = form.headers;
+        headers["accept"] = "application/vnd.api+json";
+        headers["Authorization"] = "Bearer " + api_key;
+
+        WWW request = new WWW(URL,null, headers);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;              
+        json = req.text;
+        File.WriteAllText(Application.dataPath + "listaJson.json", json);
+
+        json = File.ReadAllText(Application.dataPath + "listaJson.json");        
+       
+        JSONNode data = JSON.Parse(json);
+
+        pagCount = (data["data"].Count / 10)+1;
+        actualPageText.text = (pag + 1).ToString();
+        countPageText.text = (pagCount).ToString();
+        
+        
+        JSONArray list = data["data"].AsArray;
+        ListaTorneos = new List<Lista>();
+        foreach (JSONNode record in data["data"])
+        {
+            Lista lista = new Lista();
+            lista.id = record["id"].Value;
+            lista.createdAt = record["attributes"]["createdAt"];
+            ListaTorneos.Add(lista);          
+           
+                    
+        }
+       
+        getLista();
+    }
+    public void getLista()
+    {
+        int count = 0;
+        int i = 0;
+        destroyAllText();
+        
+
+        foreach (Lista item in ListaTorneos)
+        {
+            if (count>= pag * 10 && count < (pag * 10) + 10)
+            {
+                if (i < 10)
+                {
+                    Transform listaTransform = Instantiate(listaTemplate, listaContainer);
+                    listaTransform.tag = "ListaContainer";
+                    RectTransform listaRectTransform = listaTransform.GetComponent<RectTransform>();
+                    listaRectTransform.anchoredPosition = new Vector2(0, -60f * i);
+                    listaTransform.Find("IdTorneo").GetComponent<Text>().text = item.id;
+                    listaTransform.Find("FechaCreacion").GetComponent<Text>().text = item.createdAt;
+                    
+                    i++;
+                }
+                //Debug.Log("count: " + count + "item: " + item.id);
+            }
+            count++;
+
+        }
+    }
+    public void pageNext()
+    {
+        if (pag < pagCount-1)
+        {
+            pag++;
+            actualPageText.text = (pag + 1).ToString();
+            getLista();
+        }
+    }
+    public void pagePrev()
+    {
+        if (pag > 0)
+        {
+            pag--;
+            actualPageText.text = (pag + 1).ToString();
+            getLista();
+        }
+    }
+    public void destroyAllText()
+    {
+        GameObject[] texts;
+
+        texts = GameObject.FindGameObjectsWithTag("ListaContainer");
+        
+        foreach (GameObject text in texts)
+        {
+            Destroy(text);
+        }
+
+    }
+}
+
+[System.Serializable]
+public class Lista
+{
+    public string id, createdAt;    
+}
---
+++ WidowGamesTest/Assets/Scripts/ScalerUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScalerUI : MonoBehaviour
+{
+    public float x,y;
+
+    private CanvasScaler canScaler;
+    public Text resolution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canScaler = GetComponent<CanvasScaler>();
+        setXyValues();
+
+        canScaler.referenceResolution = new Vector2(x, y);
+    }
+
+    void setXyValues()
+    {
+        x = (float)Screen.currentResolution.width;
+        y = (float)Screen.currentResolution.height;
+        resolution.text = "Resolución: " + x + "x" + y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
